1cef72dd4e7d458f93f07111fe3b6f0d6b68dd5d
Wenping Cai <wenping.cai@rock-chips.com>
2024-11-09 16:17:09 +0800

    package: rockchip-rvcam: install rvcam prebuilts to target
    Change-Id: I4b348e75e653f9a32d121bf89386c351a9621231
    Signed-off-by: Wenping Cai <wenping.cai@rock-chips.com>

--- package/rockchip/rockchip-rvcam/rockchip-rvcam.mk
+++ package/rockchip/rockchip-rvcam/rockchip-rvcam.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# rockchip rvcam project
+# rockchip-rvcam project
 #
 ################################################################################
 
@@ -9,9 +9,23 @@ ROCKCHIP_RVCAM_SITE = $(TOPDIR)/../external/rvcam
 ROCKCHIP_RVCAM_SITE_METHOD = local
 ROCKCHIP_RVCAM_LICENSE = proprietary, GPL-2.0, Apache-2.0
 ROCKCHIP_RVCAM_LICENSE_FILES = licenses/LICENSE licenses/GPL-2.0 licenses/Apache-2.0
+ROCKCHIP_RVCAM_INSTALL_STAGING = YES
 
 ROCKCHIP_RVCAM_DEPENDENCIES += tinyxml2
 ROCKCHIP_RVCAM_DEPENDENCIES += rockchip-erpc
+
+# if rvcam/prebuilts/Linux does not exist, build RVCAM source version
+ifeq ($(wildcard $(ROCKCHIP_RVCAM_SITE)/prebuilts/Linux),)
+
+$(eval $(cmake-package))
+
+# otherwise, build RVCAM release version
+else
+
+define ROCKCHIP_RVCAM_INSTALL_TARGET_CMDS
+	cp -r $(@D)/prebuilts/Linux/usr/bin/* $(TARGET_DIR)/usr/bin; \
+	cp -r $(@D)/prebuilts/Linux/usr/lib/*.so* $(TARGET_DIR)/usr/lib
+endef
 
 define ROCKCHIP_RVCAM_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0644 $(@D)/components/config/xml/rvcam_config.xml \
@@ -20,4 +34,11 @@ define ROCKCHIP_RVCAM_INSTALL_INIT_SYSV
 		$(TARGET_DIR)/etc/init.d/
 endef
 
-$(eval $(cmake-package))
+define ROCKCHIP_RVCAM_INSTALL_STAGING_CMDS
+	cp -r $(@D)/prebuilts/Linux/usr/bin/* $(STAGING_DIR)/usr/bin; \
+	cp -r $(@D)/prebuilts/Linux/usr/lib/* $(STAGING_DIR)/usr/lib
+endef
+
+$(eval $(generic-package))
+
+endif
