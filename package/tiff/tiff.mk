ebae1ae55814edc58fdc41444a95d989d0807277
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-12-20 16:00:07 +0800

    tiff: Bump to buildroot upstream's 4.7.0
    Bump to:
    b4bffa0c9b package/mongodb: remove package
    
    Change-Id: Iba5e67a867ecb9383002ca3f0fbe4408db751d68
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- package/tiff/tiff.mk
+++ package/tiff/tiff.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-TIFF_VERSION = 4.6.0
-TIFF_SITE = http://download.osgeo.org/libtiff
+TIFF_VERSION = 4.7.0
+TIFF_SOURCE = tiff-$(TIFF_VERSION).tar.xz
+TIFF_SITE = https://download.osgeo.org/libtiff
 TIFF_LICENSE = tiff license
 TIFF_LICENSE_FILES = LICENSE.md
 TIFF_CPE_ID_VENDOR = libtiff
@@ -17,6 +18,7 @@ TIFF_INSTALL_STAGING = YES
 TIFF_CONF_OPTS = \
 	--disable-contrib \
 	--disable-lerc \
+	--disable-jbig \
 	--disable-tests \
 	--disable-webp
 
@@ -28,6 +30,7 @@ HOST_TIFF_CONF_OPTS = \
 	--disable-lerc \
 	--disable-libdeflate \
 	--disable-lzma \
+	--disable-jbig \
 	--disable-jpeg \
 	--disable-tests \
 	--disable-webp \
@@ -99,10 +102,6 @@ endif
 
 ifneq ($(BR2_PACKAGE_TIFF_OLD_JPEG),y)
 TIFF_CONF_OPTS += --disable-old-jpeg
-endif
-
-ifneq ($(BR2_PACKAGE_TIFF_JBIG),y)
-TIFF_CONF_OPTS += --disable-jbig
 endif
 
 ifeq ($(BR2_PACKAGE_TIFF_UTILITIES),y)
